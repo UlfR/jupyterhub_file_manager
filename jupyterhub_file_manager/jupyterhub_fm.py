ca6280c32357605dbf40b306ad9ceccb1564271c
Gleb Stiblo <errandir@gmail.com>
2019-11-25 22:05:22 +0600

    stubs for auth check

--- jupyterhub_file_manager/jupyterhub_fm.py
+++ jupyterhub_file_manager/jupyterhub_fm.py
@@ -1,3 +1,6 @@
+import pdb
+# noinspection PyUnresolvedReferences
+import posix1e
 import os
 import base64
 from jupyterhub.apihandlers import APIHandler
@@ -6,11 +9,13 @@ from notebook.services.contents.handlers import ContentsHandler
 from notebook.services.contents.handlers import CheckpointsHandler
 from notebook.services.contents.handlers import ModifyCheckpointsHandler
 from tornado import gen, web
+from tornado.log import app_log
 
 # FIXME read from settings
 BASE_ROOT = '/tmp/notebooks/'
 
 
+# FIXME trash support
 class DBNotebookManager(FileContentsManager):
     user = None
 
@@ -19,18 +24,104 @@ class DBNotebookManager(FileContentsManager):
         self.user = user
 
     @property
+    def user_name(self):
+        return self.user.name if self.user else 'anonymous'
+
+    # FIXME get group names
+    @property
+    def group_names(self):
+        return ['fixme']
+
+    @property
     def root_dir(self):
+        return BASE_ROOT
+
+    @property
+    def home_dir(self):
         return f'{BASE_ROOT}/{self.user.name}/'
 
-    # FIXME, rwsx
-    def share(self, path, user, rights='r'):
+    # FIXME chown on file
+    def set_ownership(self, os_path):
         pass
 
-    # FIXME
+    # noinspection PyMethodMayBeStatic
+    def get_acl(self, os_path):
+        acl = posix1e.ACL(file=os_path)
+        acl_str = acl.to_any_text(separator=b';').decode('utf-8')
+        return list(map(lambda a: a.split(':'), acl_str.split(';')))
+
+    # FIXME ids to names
+    # noinspection PyMethodMayBeStatic
+    def get_path_owner(self, os_path):
+        statinfo = os.stat(os_path)
+        uid = statinfo.st_uid
+        gid = statinfo.st_gid
+        return [uid, gid]
+
+    def is_path_in_root(self, os_path):
+        abs_path = os.path.abspath(os_path)
+        return os.path.commonprefix([self.root_dir, abs_path]) == self.root_dir
+
+    def is_path_in_home(self, os_path):
+        abs_path = os.path.abspath(os_path)
+        return os.path.commonprefix([self.home_dir, abs_path]) == self.home_dir
+
+    def get_path_permissions(self, os_path):
+        user_name = self.user_name
+        user_groups = self.group_names
+        owner_user, owner_group = self.get_path_owner(os_path)
+        acls = self.get_acl(os_path)
+
+        result = '---'
+        for acl in acls:
+            acl_type, acl_object, acl_permissions = acl
+            if acl_object == '' and acl_type == 'user' and user_name == owner_user:
+                result = acl_permissions
+                break
+            if acl_object == '' and acl_type == 'group' and (owner_group in user_groups):
+                result = acl_permissions
+                break
+            if acl_type == 'user' and user_name == acl_object:
+                result = acl_permissions
+                break
+            if acl_type == 'group' and (acl_object in user_groups):
+                result = acl_permissions
+                break
+            if acl_type == 'other':
+                result = acl_permissions
+                break
+
+        return {
+            'r': result[0] == 'r',
+            'w': result[1] == 'w',
+            'x': result[2] == 'x',
+        }
+
+    def is_path_accessible_for(self, os_path, access_type):
+        if not self.is_path_in_root(os_path):
+            return False
+
+        permissions = self.get_path_permissions(os_path)
+        return permissions[access_type]
+
+    def is_path_accessible_for_read(self, os_path):
+        return self.is_path_accessible_for(os_path, 'r')
+
+    def is_path_accessible_for_write(self, os_path):
+        return self.is_path_accessible_for(os_path, 'w')
+
+    def is_path_accessible_for_execute(self, os_path):
+        return self.is_path_accessible_for(os_path, 'x')
+
+    # FIXME, add/change acl
+    def share(self, path, user, rights='r--'):
+        pass
+
+    # FIXME, remove acl
     def unshare(self, path, user):
         pass
 
-    #FIXME check user rights
+    # FIXME check user rights
     def get_raw(self, path):
         path = path.strip('/')
         os_path = self._get_os_path(path)
@@ -41,13 +132,71 @@ class DBNotebookManager(FileContentsManager):
         # return content.decode('utf8'), 'text'
         return content
 
-    #FIXME check user rights
+    # FIXME check user rights
     def save_raw(self, path, content):
         path = path.strip('/')
         os_path = self._get_os_path(path)
-        #FIXME mkdir`s
+        # FIXME mkdir`s
         with self.atomic_writing(os_path, text=False) as f:
             f.write(content)
+
+    def _read_notebook(self, os_path, as_version=4):
+        if not self.is_path_accessible_for_read(os_path):
+            raise web.HTTPError(403, 'Permission denied')
+        return super()._read_notebook(os_path, as_version)
+
+    def _read_file(self, os_path, format):
+        if not self.is_path_accessible_for_read(os_path):
+            raise web.HTTPError(403, 'Permission denied')
+        return super()._read_file(os_path, format)
+
+    # FIXME if exists then check for write, if not then check for write on dir
+    def _save_notebook(self, os_path, nb):
+        if not self.is_path_accessible_for_write(os_path):
+            raise web.HTTPError(403, 'Permission denied')
+        return super()._save_notebook(os_path, nb)
+
+    # FIXME if exists then check for write, if not then check for write on dir
+    def _save_file(self, os_path, content, format):
+        if not self.is_path_accessible_for_write(os_path):
+            raise web.HTTPError(403, 'Permission denied')
+        return super()._save_file(os_path, content, format)
+
+    # FIXME if exists then check for write, if not then check for write on dir
+    def _copy(self, src, dest):
+        if not self.is_path_accessible_for_read(src):
+            raise web.HTTPError(403, 'Permission denied')
+        if not self.is_path_accessible_for_write(dest):
+            raise web.HTTPError(403, 'Permission denied')
+        return super()._copy(src, dest)
+
+    # FIXME
+    def _save_directory(self, os_path, model, path=''):
+        pass
+
+    # FIXME
+    def delete_file(self, path):
+        pass
+
+    # FIXME
+    def rename_file(self, old_path, new_path):
+        pass
+
+    # FIXME + param to not raise 403 but return None or use _base_model
+    def _notebook_model(self, path, content=True):
+        pass
+
+    # FIXME + param to not raise 403 but return None or use _base_model
+    def _file_model(self, path, content=True, format=None):
+        pass
+
+    # FIXME + param to not raise 403 but return None or use _base_model
+    def _dir_model(self, path, content=True):
+        pass
+
+    # FIXME + param to not raise 403 but return None or use _TYPE_model
+    def _base_model(self, path):
+        pass
 
 
 # noinspection PyAbstractClass
--- jupyterhub_file_manager/jupyterlab_fm_proxy.py
+++ jupyterhub_file_manager/jupyterlab_fm_proxy.py
@@ -1,11 +1,7 @@
-# import pdb
-# import json
-import socket
 from os import environ
 from tornado import gen, web
 from tornado.log import app_log
 from tornado.web import HTTPError
-# from jupyter_client.jsonutil import date_default
 from notebook.utils import url_path_join, maybe_future
 from notebook.base.handlers import APIHandler, path_regex
 import requests
@@ -13,32 +9,22 @@ import requests
 
 # noinspection PyAbstractClass
 class ProxyHandler(APIHandler):
-    # FIXME
     @gen.coroutine
     def proxy(self):
         request = self.request
         body = request.body
-        # pdb.set_trace()
-
         hub_api_url = environ.get('JUPYTERHUB_API_URL')
         url = url_path_join(hub_api_url, 'notebooks/'+request.uri.split('contents/')[1])
-
-        # noinspection PyProtectedMember
         if request.method == "GET":
             response = yield maybe_future(self._api_request(method=request.method, url=url))
         else:
             response = yield maybe_future(self._api_request(method=request.method, url=url, data=body))
-
         self.set_status(response.status_code)
-        #self.finish(json.dumps(model['data'], default=date_default))
         self.finish(response.text)
 
     @gen.coroutine
     def _api_request(self, method, url, **kwargs):
-        """Make an API request"""
         hub_auth = self.application.settings['hub_auth']
-        # allow_404 = kwargs.pop('allow_404', False)
-        # with_status = kwargs.pop('with_status', False)
         headers = kwargs.setdefault('headers', {})
         headers.setdefault('Authorization', 'token %s' % hub_auth.api_token)
         if "cert" not in kwargs and hub_auth.certfile and hub_auth.keyfile:
@@ -50,15 +36,6 @@ class ProxyHandler(APIHandler):
         except requests.ConnectionError as e:
             app_log.error("Error connecting to %s: %s", hub_auth.api_url, e)
             msg = "Failed to connect to Hub API at %r." % hub_auth.api_url
-            msg += (
-                "  Is the Hub accessible at this URL (from host: %s)?"
-                % socket.gethostname()
-            )
-            if '127.0.0.1' in hub_auth.api_url:
-                msg += (
-                    "  Make sure to set c.JupyterHub.hub_ip to an IP accessible to"
-                    + " single-user servers if the servers are not on the same host as the Hub."
-                )
             raise HTTPError(500, msg)
         return r
 
